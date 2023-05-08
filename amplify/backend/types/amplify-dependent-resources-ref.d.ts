739ccaf3d6020b55e6cabb4a4eb61c98b45c024e
Margo <marglodistovar@gmail.com>
2023-05-08 18:24:35 -0400

    Amplify storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "grupocuatrostorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
